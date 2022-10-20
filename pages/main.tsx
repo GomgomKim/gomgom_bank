43791d4816cbf416eb270e8d508adb55510d657f
GomgomKim <dhflwlsjfzz@naver.com>
2022-10-20 23:52:48 +0900

    [Add Create Authorize Form in main page] 1. Add Create Authorize Form in main page

--- pages/main.tsx
+++ pages/main.tsx
@@ -6,9 +6,16 @@ function Main({ Component, pageProps }: AppProps) {
   return <>
     <div className={styles.background}>
       <div className={styles.overlay}>
-        <h2>코딩 마차</h2>
-        <h3>이미지 컴포넌트로 배경처리하기</h3>
       </div>
+      <form method="get" action="https://testapi.openbanking.or.kr/oauth/2.0/authorize" target="_blank">
+        <input type="hidden" name="response_type" value="code"/>
+        <input type="hidden" name="client_id" value="eb821e42-3410-4dec-98f2-f2a8cc40af5d"/>
+        <input type="hidden" name="redirect_uri" value="http://localhost:3000/"/>
+        <input type="hidden" name="scope" value="login inquiry transfer"/>
+        <input type="hidden" name="state" value="b80BLsfigm9OokPTjy03elbJqRHOfGSY"/>
+        <input type="hidden" name="auth_type" value="0"/>
+        <input type="submit" value="requestAuth"/>
+      </form>
       <Image
         src="/images/blockchain_back.jpg"
         alt="메인 배경 이미지"
