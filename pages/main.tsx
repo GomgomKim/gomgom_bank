af506344546a670419fcff9c2c88b11f8abb2104
Gomgomkim <dhflwlsjfzz@naver.com>
2022-10-21 14:31:35 +0900

    [Create Main Page css module]

--- pages/main.tsx
+++ pages/main.tsx
@@ -1,13 +1,11 @@
 import type { AppProps } from 'next/app'
 import Image from "next/image";
-import styles from 'styles/main.module.css';
+import styles from '../styles/main.module.css';
 
 function Main({ Component, pageProps }: AppProps) {
   return <>
     <div className={styles.background}>
-      <div className={styles.overlay}>
-      </div>
-      <form method="get" action="https://testapi.openbanking.or.kr/oauth/2.0/authorize" target="_blank">
+      <form method="get" action="https://testapi.openbanking.or.kr/oauth/2.0/authorize" target="_blank" style={{zIndex: 1}}>
         <input type="hidden" name="response_type" value="code"/>
         <input type="hidden" name="client_id" value="eb821e42-3410-4dec-98f2-f2a8cc40af5d"/>
         <input type="hidden" name="redirect_uri" value="http://localhost:3000/"/>
@@ -17,6 +15,7 @@ function Main({ Component, pageProps }: AppProps) {
         <input type="submit" value="requestAuth"/>
       </form>
       <Image
+        className={styles.backgoundImage}
         src="/images/blockchain_back.jpg"
         alt="메인 배경 이미지"
         layout="fill"
